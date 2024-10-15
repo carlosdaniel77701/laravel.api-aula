c1ef82f355a310c26f69c7625187344e7548672a
Carlos Daniel <104913877+carlosdaniel77701@users.noreply.github.com>
2024-10-15 09:28:44 -0300

    1510

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@ Route::get('/', function () {
 });
 
 Route::get('/somar', function(Request $request){
+    // não está chegando nada pela request
+    if (count($request->all()) < 1) {
+        return response()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406);
+    }
     $soma = array_sum($request->all());
     return response()->json([
         'message' => 'Somado com sucesso',
