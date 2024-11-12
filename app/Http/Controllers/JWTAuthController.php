785efa17d2e1118a689a0030b9e3394fd8993625
Carlos Daniel <104913877+carlosdaniel77701@users.noreply.github.com>
2024-11-12 08:46:17 -0300

    1211

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+
+use function PHPUnit\Framework\returnSelf;
 
 class JWTAuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request){
         $data = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
@@ -23,28 +26,31 @@ class JWTAuthController extends Controller
         ]);
 
         $token = JWTAuth::fromUser($user);
+        return response()->json(compact('user','token'), 201);
 
-        return response()->json(compact("user","token"),201);
     }
-    public function login(Request $request) {
-        $cred = $request->only('email','password');
 
-        try {
-            // se o login não funcionar 
-            if (! JWTAuth::attempt($cred)) {
+    public function login(Request $request){
+        $cred = $request->only('email', 'password');
+
+        try{
+            if(!JWTAuth::attempt($cred)){
                 return response()->json(['error' => 'Invalid credentials'], 401);
             }
+
+            $user = JWTAuth::user();
+
+            $token = JWTAuth::fromUser($user);
+
+            return response()->json(compact('token'));
+        
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
         }
-        //Se chegou aqui, deu certo!
-        $user = auth()->user();
-
-        //no caso de atribuir um papel
-        // $token = JWTAuth::claims(['role' => $user->role])->fromUser($user);
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json(compact('token'));
-    } catch (JWTException $e) {
-        return response()->json(['error' => 'Could not create token'], 500);
     }
+
+    public function logout() {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Logout succesfuly'], 200);
     }
 }
